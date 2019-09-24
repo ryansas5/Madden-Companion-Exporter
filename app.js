0a0c8bb73204499fd270cfab0216b2039a263c81
ryansas5 <55729047+ryansas5@users.noreply.github.com>
2019-09-24 00:42:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./lions-franchise.json");
+// const serviceAccount = require("./lions-franchise-firebase-adminsdk-a9tl3-0c56b56c70.json");
 
 // TODO: Enter your database url from firebase
 // admin.initializeApp({
